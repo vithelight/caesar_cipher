10f3e11e1862ae9663683c0e12a9ab9ba1ba25a9
Vi <150180996+vithelight@users.noreply.github.com>
2023-12-08 14:59:23 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+from art import logo
+import re
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+regexp = re.compile('[^0-9a-zA-Z]+')
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char.isspace() or char.isdecimal() or regexp.search(char):
+      end_text += char
+    else:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    
+  print(f"\nHere's the {cipher_direction}d result: {end_text}")
+
+print(logo)
+
+while True:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  
+  if shift > 26:
+    shift = shift % 26
+  
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  restart = input("Do you want to restart the cipher program? 'yes' or 'no'. \n").lower()
+  if restart == 'no':
+    break
